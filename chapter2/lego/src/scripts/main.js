149a0b2db7b70589e0c6d64988c073b1766b0160
Ryan Dillinger <rd.nss.services@gmail.com>
2019-02-01 13:33:43 -0600

    continued work on Lego x

--- chapter2/lego/src/scripts/main.js
+++ chapter2/lego/src/scripts/main.js
@@ -3,25 +3,28 @@
 // Some starter code
 document.querySelector("#lego__save").addEventListener("click", event => {
     const creator = document.querySelector("#lego__creator").value
-    const color = document.querySelector("#lego__color").value
+    const colorKey = document.querySelector("#lego__color").value
     const shape = document.querySelector("#lego__shape").value
     const creation = document.querySelector("#lego__creation").value
-
-
-    // Once you have collected all the values, build your data structure
-    let legoToSave = {
-        "creator": `${creator}`,
-        "color": `${color}`,
-        "shape": `${shape}`,
-        "creation": `${creation}`
-    }
-
-
-    fetch("http://localhost:3000/legoCreation", { // Replace "url" with your API's URL
+    
+  
+    fetch("http://localhost:3000/colors") // Replace "url" with your API's URL
+        .then(response => response.json())
+        .then(colors => {
+            ggg= colors[colorKey]
+            let legoToSave = {
+                "creator": `${creator}`,
+                "color": `${ggg}`,
+                "shape": `${shape}`,
+                "creation": `${creation}`
+            }
+        fetch("http://localhost:3000/legoCreation", { // Replace "url" with your API's URL
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(legoToSave)
+        
     })
-})
+})})
+
